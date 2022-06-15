4ccf9a7c944a77abf6d6cc08c06311aefa1c79ba
Lanny MacMillan <LannyMacMillan@gmail.com>
2022-06-15 16:06:16 -0400

    heroku added to axois

--- game_frontend/src/App.js
+++ game_frontend/src/App.js
@@ -11,7 +11,8 @@ function App() {
 
   const getAnimals = () => {
     axios
-        .get('http://localhost:8000/api/species')
+        .get('https://rocky-hollows-96922.herokuapp.com/api/species')
+        // .get('http://localhost:8000/api/species')
         .then(response => setAnimals(response.data),
         (err)=> console.error(err)
         )
@@ -19,7 +20,8 @@ function App() {
   }
   const handleCreate = (addAnimal) => {
     axios
-      .post('http://localhost:8000/api/species', addAnimal)
+      .post('https://rocky-hollows-96922.herokuapp.com/api/species', addAnimal)
+      // .post('http://localhost:8000/api/species', addAnimal)
       .then((response) => {
         // takes the existing state and spreads it, adds new object to the end
         setAnimals([...animals, response.data])
@@ -30,7 +32,8 @@ function App() {
   const handleUpdate =(editAnimal) => {
     axios   
     // id updates ID in DB, editComic brings the info from that function
-      .put('http://localhost:8000/api/species/' + editAnimal.id, editAnimal)
+      .put('https://rocky-hollows-96922.herokuapp.com/api/species/' + editAnimal.id, editAnimal)
+      // .put('http://localhost:8000/api/species/' + editAnimal.id, editAnimal)
       .then((response) => {
         setAnimals(animals.map((animal) => {
           return animal.id !== response.data.id ? animal : response.data
@@ -40,7 +43,8 @@ function App() {
   }
   const handleDelete = (deletedAnimal) => {
     axios
-      .delete('http://localhost:8000/api/species/' + deletedAnimal.id)
+      .delete('https://rocky-hollows-96922.herokuapp.com/api/species/' + deletedAnimal.id)
+      // .delete('http://localhost:8000/api/species/' + deletedAnimal.id)
       .then((response) => {
         setAnimals(animals.filter(animal => animal.id !== deletedAnimal.id))
         // getAnimals()
